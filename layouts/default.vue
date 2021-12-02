9d6352ee1681fbbd1770095c7361f1d80b9c48dc
Sultenhest <simon@konstantyner.dk>
2021-12-02 21:28:05 +0100

    dghkdg

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,6 @@
 
     <main id="main" class="max-w-screen-lg mx-auto">
       <Nuxt />
-      <select
-        v-model="$colorMode.preference"
-        class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700 mb-10"
-      >
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
     </main>
   </div>
 </template>
